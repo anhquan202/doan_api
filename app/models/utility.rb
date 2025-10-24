002f9bec69270dd212fec594cc5767ef76757f1e
anhquan202 <anhquan18012002@gmail.com>
2025-10-24 16:25:10 +0000

    update serializer

--- app/models/utility.rb
+++ app/models/utility.rb
@@ -3,4 +3,8 @@ class Utility < ApplicationRecord
   has_many :rooms, through: :room_utilities
 
   enum :utility_type, electricity: 0, water: 1, internet: 2, parking: 3, garbage: 4, cleaning: 5
+
+  def utility_type_label
+    I18n.t("enums.utility.utility_type.#{utility_type}")
+  end
 end
--- app/serializers/utility_serializer.rb
+++ app/serializers/utility_serializer.rb
@@ -1,5 +1,5 @@
 class UtilitySerializer < ActiveModel::Serializer
-  attributes :id, :utility_type, :description, :is_required
+  attributes :id, :utility_type, :utility_type_label, :description, :is_required
 
   def is_required
     instance_options[:room_utility]&.is_required
