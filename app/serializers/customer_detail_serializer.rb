19da5aedcf4ae7cfe6cab95bba1f5544b7d51bcb
anhquan202 <anhquan18012002@gmail.com>
2025-11-27 16:05:27 +0000

    [doan_api/issues/46] API detail customer

--- app/serializers/customer_detail_serializer.rb
+++ app/serializers/customer_detail_serializer.rb
@@ -1,5 +1,5 @@
-class CustomerSerializer < ActiveModel::Serializer
-  attributes :id, :identity_code, :full_name, :email, :phone, :address, :gender, :date_of_birth, :status
+class CustomerDetailSerializer < ActiveModel::Serializer
+  attributes :id, :email, :phone, :address, :gender, :date_of_birth, :room_name
 
   attribute :gender_text do
     I18n.t("enums.customer.gender.#{object.gender}", locale: :vi)
@@ -7,5 +7,9 @@ class CustomerSerializer < ActiveModel::Serializer
 
   attribute :status_text do
     I18n.t("enums.customer.status.#{object.status}", locale: :vi)
+  end
+
+  def room_name
+    object.contract&.room_name
   end
 end
