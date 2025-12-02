954129c2b700edec61f2a9cb6aad7bc741e20623
anhquan202 <anhquan18012002@gmail.com>
2025-12-02 14:19:53 +0000

    Refactor contract detail serializer

--- app/serializers/contract_detail_serializer.rb
+++ app/serializers/contract_detail_serializer.rb
@@ -1,5 +1,5 @@
 class ContractDetailSerializer < ActiveModel::Serializer
-  attributes :id, :start_date, :term_months, :deposit, :status, :room_info
+  attributes :id, :start_date, :contract_code, :room_info
 
   has_many :customers, serializer: CustomerSerializer
 
@@ -8,7 +8,14 @@ class ContractDetailSerializer < ActiveModel::Serializer
       id: object.room.id,
       name: object.room_name,
       supplies: object.room.supplies,
-      utilities: object.utilities
+      utilities: serialized_utilities
     }
+  end
+
+  private
+  def serialized_utilities
+    object.utilities.map do |u|
+      UtilitySerializer.new(u, scope: scope, root: false).as_json
+    end
   end
 end
